db477e951c2a875ec0dbd977bb0de8bc095804b5
Mariano <marianopondal@gmail.com>
2014-12-12 10:58:16 -0300

    primer commit

---
+++ js/slide.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //Navigation
+        menu: '#myMenu',
+        anchors:['slide1','slide2','slide3','slide4'],
+        navigation: false,
+        navigationPosition: 'right',
+        navigationTooltips: ['firstSlide', 'secondSlide'],
+        slidesNavigation: true,
+        slidesNavPosition: 'top',
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        scrollBar: false,
+        easing: 'easeInQuart',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+
+        //Design
+        verticalCentered: true,
+        resize : true,
+        sectionsColor : ['#ccc', '#fff'],
+        paddingTop: '0px',
+        paddingBottom: '0px',
+        fixedElements: '#header, .footer',
+        responsive: 900,
+
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction){}
+    });
+    
+});
